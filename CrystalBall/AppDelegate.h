77dc4dce15c1ac33edc1823bcf137262114fd5cb
ag12 <amirg12@hotmail.no>
2014-10-26 14:58:22 +0100

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Amir Ghoreshi on 26/10/14.
+//  Copyright (c) 2014 Netlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Amir Ghoreshi on 26/10/14.
+//  Copyright (c) 2014 Netlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
